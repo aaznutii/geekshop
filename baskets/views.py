c56ed7010fe4ec6b1556ce7c0ea4183aa977ce6d
aaznutii <76644651+aaznutii@users.noreply.github.com>
2021-11-27 18:58:22 +0400

    base complete less_6

--- baskets/views.py
+++ baskets/views.py
@@ -1,10 +1,15 @@
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.shortcuts import HttpResponseRedirect
+from django.template.loader import render_to_string
+
 from baskets.models import Basket
 from products.models import Product
 
-# Create your views here.
 
+# Create your views here.
+@login_required
 def basket_add(request, id):
     user_select = request.user
     product = Product.objects.get(id=id)
@@ -18,6 +23,24 @@ def basket_add(request, id):
         Basket.objects.create(user=user_select, product=product, quantity=1)
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
+
+@login_required
 def basket_remove(request, id):
     Basket.objects.get(id=id).delete()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+@login_required
+def basket_edit(request, id_basket, quantity):
+    if request.is_ajax():
+        basket = Basket.objects.get(id=id_basket)
+        if quantity > 0:
+            basket.quantity = quantity
+            basket.save()
+        else:
+            basket.delete()
+
+        baskets = Basket.objects.filter(user=request.user)
+        context = {'baskets': baskets}
+        result = render_to_string('baskets/baskets.html', context)
+        return JsonResponse({'result': result})
